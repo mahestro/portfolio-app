79fe8fc85739a099c388d8c2fba1f17a95e9b1e0
mahestro <millanluiseduardo@gmail.com>
2018-07-28 16:51:02 +0200

    scss clean up - ready to start

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Header
+        <div className="esta">
+          Aja!
+        </div>
         Footer
       </div>
     );
